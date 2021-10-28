bf63917533d68027092837b12a224c6474a2c5f0
‘Katie <ktschr11@gmail.com>
2021-10-28 09:47:57 -0700

    added readJSON test and method

--- __tests__/SimpleDB.test.js
+++ __tests__/SimpleDB.test.js
@@ -8,16 +8,25 @@ describe('SimpleDB', () => {
   beforeEach(() => {
     return rm(rootDir, { force: true, recursive: true }).then(() => mkdir(rootDir, { recursive: true }));
   });
-  //save test
-  it ('creates and saves an object in the root Dir', () => {
-    const DB = new SimpleDB(rootDir);
-    const object = { message: 'silly lil string', from: 'who knows' };
 
-    const dirContents = readdir(rootDir);
+  //save test
+  it ('creates and save, and get an object in the root Dir', () => {
+
+    const DB = new SimpleDB(rootDir);
+
+    const object = { 
+      message: 'silly lil string', 
+      from: 'who knows' 
+    };
+
+    const expected = Object.assign({
+      id: expect.any(String)
+    }, object);
 
     return DB
       .save(object)
-      .then(() => expect(dirContents).toBeTruthy());
+      .then(() => DB.get(object.id))
+      .then(actual => expect(actual).toEqual(expected));
   });
 });
 
